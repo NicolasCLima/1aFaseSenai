57d95d05e874f54bc13228f3754d4a1f8d573d7b
Nicolas C. Lima <166423644+NicolasCLima@users.noreply.github.com>
2024-12-10 19:08:48 -0300

    Add files via upload

---
+++ ProjetoSA/card.js
@@ -0,0 +1,12 @@
+function saibaMais1(){
+    
+    window.location.href= "index.html";
+}
+
+function saibaMais2(){
+    window.location.href= "calcMacro.html"
+}
+
+function SaibaMais3(){
+    window.location.href= "calcCalo.html"
+}
